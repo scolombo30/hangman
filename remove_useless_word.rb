d404fc1438770d87745fba9d18d3571a0712290b
Sebastian <scolombo30@studenti.uninsubria.it>
2022-09-19 15:55:28 +0200

    Add script that remove useless word(legnth not in 5..12)

---
+++ remove_useless_word.rb
@@ -0,0 +1,11 @@
+file = File.new('dictionary.txt')
+
+words = file.readlines
+
+words.select!{|el| el.chomp.length.between?(5,12)}
+
+File.delete('dictionary.txt')
+file = File.new('dictionary.txt','w')
+length = words.size
+words.each_with_index {|el, index| index == length - 1 ? file.write(el.chomp) : file.write(el)}
+file.close
